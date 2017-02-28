79c260426a0a0433dc0d3879eea3c1a2fed653e4
Brendan Thorn <brendankthorn@gmail.com>
2017-02-27 22:21:37 -0800

    Updated Bookie.py, made some small changes to output of RandSelectSimulations

--- OddsPrediction/Bookie.py
+++ OddsPrediction/Bookie.py
@@ -1,26 +1,39 @@
-
+import numpy as np
 
 class Bookie(object):
 
 	def __init__(self, home_prob, goodness, balance=0, silent=False,):
 		self.balance = balance
-		self.wins = 0
-		self.losses = 0
+		self.num_wins = 0
+		self.num_losses = 0
 		self.silent = silent
 		self.home_prob = home_prob
 		self.goodness = goodness
 
-	def place_bet(self,pick,winner,odds,bet):
+	def place_bet(self,pick,winner,odds,bet=1):
+		"""
+		For single bets.
+		"""
 		self.update_balance(-bet)
 		if pick == winner:
-			self.wins += 1
+			self.num_wins += 1
 			self.update_balance(odds*bet)
 			if not self.silent:
 				print "W! Bet on:", pick, " Winner:", winner, " Odds:", odds, " Net:", odds*bet, " Balance:", self.balance
 		elif pick != winner:
-			self.losses += 1
+			self.num_losses += 1
 			if not self.silent:
 				print "L! Bet on:", pick, " Winner:", winner, " Odds:", odds, " Net:", -bet, " Balance:", self.balance
+
+	def place_bets(self,picks,winners,odds,bet=1):
+		"""
+		For multiple, vectorized bets.
+		"""
+		wins = (picks == winners).astype(int)
+		self.num_wins = sum(wins)
+		self.num_losses = len(wins) - self.num_wins
+		profits = np.multiply(np.multiply(wins,odds),bet)-bet
+		self.update_balance(sum(profits))
 
 	def update_balance(self,amount):
 		self.balance += amount
@@ -30,8 +43,8 @@ class Bookie(object):
 		print "Bet timing skill:", self.goodness
 		print "Pick home prob.:", self.home_prob
 		print "Final balance:", self.balance
-		print "Number placed:", self.wins+self.losses
-		print "Final wins:", self.wins
-		print "Final losses:", self.losses
+		print "Number placed:", self.num_wins+self.num_losses
+		print "Final wins:", self.num_wins
+		print "Final losses:", self.num_losses
 		print "#######################################\n"
 
--- OddsPrediction/RandSelectSimulations.py
+++ OddsPrediction/RandSelectSimulations.py
@@ -7,37 +7,42 @@ is likely to have.
 import MySQLdb, re, time, sys, math, signal
 import numpy as np
 import pandas as pd
-pd.set_option('display.max_rows', 100)
+pd.set_option('display.max_rows', 30)
 pd.set_option('display.max_columns', 20)
 pd.set_option('display.width', 1000)
 import HalcyonNHLdb
-import GamesByDate
 import Bookie
 
 
 class SeasonData(object):
 
-	def __init__(self,season,books=['Pinnacle']):
+	def __init__(self,season,books=['Pinnacle','SportsInteraction']):
 		self.db = HalcyonNHLdb.HalcyonNHLdb()
 		self.season = season
 		self.books = books
 		self.odds_data = self.get_odds_data()
-		self.homeaway_data = self.get_homeaway_data()
-		self.games = GamesByDate.GamesByDate(season)
-
-	def get_homeaway_data(self):
-		query = "SELECT gameID, date, team as home, opponent as away, winner FROM Games{0} WHERE location=\"home\"".format(self.season)
-		homeaway_data = pd.read_sql(query,con=self.db.get_connection())
-		return homeaway_data
+		self.home_data = self.odds_data.loc[self.odds_data["team"]==self.odds_data["home"]]
+		self.home_data = self.home_data.reset_index(drop=True)
+		self.away_data = self.odds_data.loc[self.odds_data["team"]==self.odds_data["away"]]
+		self.away_data = self.away_data.reset_index(drop=True)
 
 	def get_odds_data(self):
-		# Get all data for the season
 		if len(self.books) == 1:
-			query = "SELECT gameID, date, team, max(odds) as max, min(odds) as min FROM Moneylines{0} WHERE bookName=\"{1}\" GROUP BY gameID,team,date;".format(self.season,self.books[0])
-		else:
-			query = "SELECT gameID, date, team, max(odds) as max, min(odds) as min FROM Moneylines{0} WHERE bookName in {1} GROUP BY gameID,team,date;".format(self.season,tuple(self.books))
-		odds_data = pd.read_sql(query,con=self.db.get_connection())
-		return odds_data
+			query = "SELECT t2.gameID, t2.date, t2.team as team, t1.team as home, t1.opponent as away,\
+				t1.winner as winner, t2.max as max, t2.min as min FROM (SELECT gameID, date, team,\
+				opponent, winner FROM Games{0} WHERE location=\"home\") AS t1 RIGHT JOIN (SELECT\
+				gameID, date, team, max(odds) AS max, min(odds) AS min FROM Moneylines{0} WHERE\
+				bookName=\"{1}\" GROUP BY gameID, team, date) AS t2 ON t1.gameID=t2.gameID AND\
+				t1.date=t2.date;".format(self.season,self.books[0])
+		elif len(self.books) > 1:
+			query = "SELECT t2.gameID, t2.date, t2.team as team, t1.team as home, t1.opponent as away,\
+				t1.winner as winner, t2.max as max, t2.min as min FROM (SELECT gameID, date, team,\
+				opponent, winner FROM Games{0} WHERE location=\"home\") AS t1 RIGHT JOIN (SELECT\
+				gameID, date, team, max(odds) AS max, min(odds) AS min FROM Moneylines{0} WHERE\
+				bookName in {1} GROUP BY gameID, team, date) AS t2 ON t1.gameID=t2.gameID AND\
+				t1.date=t2.date;".format(self.season,tuple(self.books))
+		data = pd.read_sql(query,con=self.db.get_connection())
+		return data.dropna()
 
 
 class SimulateSeason(object):
@@ -47,61 +52,52 @@ class SimulateSeason(object):
 		self.bookie = bookie
 		self.home_prob = home_prob
 		self.goodness = goodness
+		self.simulate()
 
 	def simulate(self):
-		for game_date in self.data.games.game_dates:
-			for game_id in self.data.games.gbd[game_date]:
-				# Randomly select pick
-				pick = self.select_pick(game_id)
-				# Get odds you want (0=worst recorded, 1=best recorded)
-				odds = self.select_odds(game_id,game_date,pick)
-				if odds < 0:
-					continue
-				# Find out actual winner
-				winner = self.get_winner(game_id,game_date)
-				# Place bet
-				self.bookie.place_bet(pick,winner,odds,1)
+		# Decife if we pick home team to win for each game in data
+		pick_home = (np.random.uniform(size=len(self.data.odds_data.index)/2) < self.home_prob)
+		# Get picks from corresponding rows of from home_data and away_data frames
+		home_picks = self.data.home_data.loc[pick_home]
+		away_picks = self.data.away_data.loc[~pick_home]
+		picks = pd.concat([home_picks,away_picks])
+		picks = picks.sort_values(["date","gameID"])
+		# Calculate and add column of odds based on goodness of skill
+		picks["odds"] = picks["min"] + self.goodness*(picks["max"]-picks["min"])
+		# Place bets
+		self.bookie.place_bets(picks["team"].values,picks["winner"].values,picks["odds"])
 
-	def select_pick(self,game_id):
-		home_winner = (np.random.uniform() < self.home_prob)
-		if home_winner:
-			team = self.data.homeaway_data.loc[self.data.homeaway_data["gameID"].isin([game_id]),"home"].values[0]
-		else:
-			team = self.data.homeaway_data.loc[self.data.homeaway_data["gameID"].isin([game_id]),"away"].values[0]
-		return team
 
-	def select_odds(self,game_id,game_date,pick):
-		try:
-			min_odds = float(self.data.odds_data.loc[(self.data.odds_data["gameID"].isin([game_id])) & (self.data.odds_data["date"].isin([game_date])) & (self.data.odds_data["team"].isin([pick])),"min"].values[0])
-			max_odds = float(self.data.odds_data.loc[(self.data.odds_data["gameID"].isin([game_id])) & (self.data.odds_data["date"].isin([game_date])) & (self.data.odds_data["team"].isin([pick])),"max"].values[0])
-		except:
-			return -1
-		odds = min_odds + self.goodness*(max_odds-min_odds)
-		return odds
+class BuildHistograms(object):
 
-	def get_winner(self,game_id,game_date):
-		winner = self.data.homeaway_data.loc[(self.data.homeaway_data["gameID"].isin([game_id])) & (self.data.homeaway_data["date"].isin([game_date])),"winner"].values[0]
-		return winner
+	def __init__(self,season,home_prob,goodness,silent,num_sims):
+		self.data = SeasonData(season)
+		self.home_prob = home_prob
+		self.goodness = goodness
+		self.silent = silent
+		self.num_sims = num_sims
+		self.earnings = []
+		self.execute()
 
-def execute(season,home_prob,goodness,silent,num_sims):
-
-	sd = SeasonData(season)
-	earnings = []
-	for i in range(num_sims):
-		print "Simulation", i+1, "of", num_sims
-		bk = Bookie.Bookie(home_prob,goodness,silent=silent)
-		ss = SimulateSeason(sd,bk,home_prob,goodness)
-		ss.simulate()
-		earnings.append(bk.balance)
-		bk.summary()
-	print "Average:", sum(earnings)/len(earnings), " Best:", max(earnings), " Worst:", min(earnings) 
+	def execute(self):
+		for i in range(self.num_sims):
+			print "Simulation", i+1, "of", self.num_sims,"\r",
+			bk = Bookie.Bookie(home_prob,goodness,silent=silent)
+			ss = SimulateSeason(self.data,bk,self.home_prob,self.goodness)
+			self.earnings.append(bk.balance)
+			#bk.summary()
+		earnings = np.array(self.earnings)
+		print "\nMean:", np.mean(earnings)
+		print "Quintiles [0, 20, 40, 60, 80, 100]:"
+		print np.percentile(earnings, np.arange(0, 120, 20))
+		#print "Average:", sum(self.earnings)/len(self.earnings), " Best:", max(self.earnings), " Worst:", min(self.earnings),"\n" 
 
 if __name__ == "__main__":
 	season = 20152016
 	home_prob = .5
-	goodness = 0.
+	goodness = 1.
 	silent = True
-	num_sims = 100
-	execute(season,home_prob,goodness,silent,num_sims)
+	num_sims = 10000
+	BuildHistograms(season,home_prob,goodness,silent,num_sims)
 
 
