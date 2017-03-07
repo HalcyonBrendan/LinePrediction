f28541ed154a5dcf05cd552e70c0f8a2ce9ee8d1
Brendan Thorn <brendankthorn@gmail.com>
2017-03-07 01:09:27 -0800

    Modified the program to use max of opening odds across books, rather than min of all odds across books.

--- OddsPrediction/RandSelectSimulations.py
+++ OddsPrediction/RandSelectSimulations.py
@@ -16,7 +16,7 @@ import Bookie
 
 class SeasonData(object):
 
-	def __init__(self,season,books=['Pinnacle','SportsInteraction']):
+	def __init__(self,season,books=['Pinnacle','FiveDimes','SportsInteraction','Bet365']):
 		self.db = HalcyonNHLdb.HalcyonNHLdb()
 		self.season = season
 		self.books = books
@@ -28,19 +28,32 @@ class SeasonData(object):
 
 	def get_odds_data(self):
 		if len(self.books) == 1:
-			query = "SELECT t2.gameID, t2.date, t2.team as team, t1.team as home, t1.opponent as away,\
-				t1.winner as winner, t2.max as max, t2.min as min FROM (SELECT gameID, date, team,\
-				opponent, winner FROM Games{0} WHERE location=\"home\") AS t1 RIGHT JOIN (SELECT\
-				gameID, date, team, max(odds) AS max, min(odds) AS min FROM Moneylines{0} WHERE\
-				bookName=\"{1}\" GROUP BY gameID, team, date) AS t2 ON t1.gameID=t2.gameID AND\
-				t1.date=t2.date;".format(self.season,self.books[0])
+			query = 'SELECT t1.gameID, t1.date, t1.team, t0.team as home, t0.opponent as away, t0.winner, t2.openOdds, t3.closeOdds, t4.maxOdds\
+				FROM (SELECT gameID, date, team, opponent, winner FROM Games{0} WHERE location="home") as t0\
+				INNER JOIN (SELECT gameID, date, team, min(pollTime) as openTime, max(pollTime) as closeTime FROM Moneylines{0} WHERE bookName="{1}" GROUP BY gameID, team, date) as t1\
+				ON (t0.gameID=t1.gameID AND t0.date=t1.date)\
+				INNER JOIN (SELECT gameID, date, team, pollTime, max(odds) as openOdds FROM Moneylines{0} WHERE bookName="{1}" GROUP BY gameID, date, team, pollTime) as t2\
+				ON (t1.gameID=t2.gameID AND t1.date=t2.date AND t1.team=t2.team AND t1.openTime=t2.pollTime)\
+				INNER JOIN (SELECT gameID, date, team, pollTime, max(odds) as closeOdds FROM Moneylines{0} WHERE bookName="{1}" GROUP BY gameID, date, team, pollTime) as t3\
+				ON t1.gameID=t3.gameID AND t1.date=t3.date AND t1.team=t3.team AND t1.closeTime=t3.pollTime\
+				INNER JOIN (SELECT gameID, date, team, max(odds) AS maxOdds FROM Moneylines{0} WHERE bookName="{1}" GROUP BY gameID, team, date) as t4\
+				ON t1.gameID=t4.gameID AND t1.date=t4.date AND t1.team=t4.team\
+				ORDER BY t0.date, t0.gameID;'\
+				.format(self.season,self.books[0])
 		elif len(self.books) > 1:
-			query = "SELECT t2.gameID, t2.date, t2.team as team, t1.team as home, t1.opponent as away,\
-				t1.winner as winner, t2.max as max, t2.min as min FROM (SELECT gameID, date, team,\
-				opponent, winner FROM Games{0} WHERE location=\"home\") AS t1 RIGHT JOIN (SELECT\
-				gameID, date, team, max(odds) AS max, min(odds) AS min FROM Moneylines{0} WHERE\
-				bookName in {1} GROUP BY gameID, team, date) AS t2 ON t1.gameID=t2.gameID AND\
-				t1.date=t2.date;".format(self.season,tuple(self.books))
+			query = 'SELECT date, gameID, team, home, away, winner, max(openOdds) as openOdds, max(closeOdds) as closeOdds, max(maxOdds) as maxOdds\
+				FROM (SELECT t1.gameID as gameID, t1.date as date, t1.team as team, t0.home as home, t0.away as away, t0.winner as winner, t1.bookName as bookName, t2.openOdds as openOdds, t3.closeOdds as closeOdds, t4.maxOdds as maxOdds\
+				FROM (SELECT gameID, date, team as home, opponent as away, winner FROM Games{0} WHERE location="home") as t0\
+				INNER JOIN (SELECT date, gameID, team, bookName, min(pollTime) as openTime, max(pollTime) as closeTime FROM Moneylines{0} WHERE bookName in {1} GROUP BY date, gameID, team, bookName) as t1\
+				ON (t0.gameID=t1.gameID AND t0.date=t1.date)\
+				INNER JOIN (SELECT gameID, date, team, bookName, pollTime, max(odds) as openOdds FROM Moneylines{0} WHERE bookName in {1} GROUP BY gameID, date, team, bookName, pollTime) as t2\
+				ON (t1.gameID=t2.gameID AND t1.date=t2.date AND t1.team=t2.team AND t1.openTime=t2.pollTime AND t1.bookName=t2.bookName)\
+				INNER JOIN (SELECT gameID, date, team, bookName, pollTime, max(odds) as closeOdds FROM Moneylines{0} WHERE bookName in {1} GROUP BY gameID, date, team, bookName, pollTime) as t3\
+				ON (t1.gameID=t3.gameID AND t1.date=t3.date AND t1.team=t3.team AND t1.closeTime=t3.pollTime AND t1.bookName=t3.bookName)\
+				INNER JOIN (SELECT gameID, date, team, max(odds) AS maxOdds FROM Moneylines{0} WHERE bookName in {1} GROUP BY gameID, team, date) as t4\
+				ON (t1.gameID=t4.gameID AND t1.date=t4.date AND t1.team=t4.team)) as a1\
+				GROUP BY a1.date, a1.gameID, a1.team, a1.home, a1.away, a1.winner;'\
+				.format(self.season,tuple(self.books))
 		data = pd.read_sql(query,con=self.db.get_connection())
 		return data.dropna()
 
@@ -63,15 +76,15 @@ class SimulateSeason(object):
 		picks = pd.concat([home_picks,away_picks])
 		picks = picks.sort_values(["date","gameID"])
 		# Calculate and add column of odds based on goodness of skill
-		picks["odds"] = picks["min"] + self.goodness*(picks["max"]-picks["min"])
+		picks["odds"] = picks["openOdds"] + self.goodness*(picks["maxOdds"]-picks["openOdds"])
 		# Place bets
 		self.bookie.place_bets(picks["team"].values,picks["winner"].values,picks["odds"])
 
 
 class BuildHistograms(object):
 
-	def __init__(self,season,home_prob,goodness,silent,num_sims):
-		self.data = SeasonData(season)
+	def __init__(self,season,books,home_prob,goodness,silent,num_sims):
+		self.data = SeasonData(season,books=books)
 		self.home_prob = home_prob
 		self.goodness = goodness
 		self.silent = silent
@@ -87,17 +100,20 @@ class BuildHistograms(object):
 			self.earnings.append(bk.balance)
 			#bk.summary()
 		earnings = np.array(self.earnings)
-		print "\nMean:", np.mean(earnings)
-		print "Quintiles [0, 20, 40, 60, 80, 100]:"
-		print np.percentile(earnings, np.arange(0, 120, 20))
+		print "\n\nBooks:",self.data.books,"Home Prob.:",self.home_prob,"Goodness:",self.goodness,"Num. Games:",len(self.data.odds_data)/2
+		print "Mean:", np.mean(earnings)
+		quintiles = np.percentile(earnings, np.arange(0, 120, 20))
+		print "Quintiles [0%: {0}, 20%: {1}, 40%: {2}, 60%: {3}, 80%: {4}, 100%: {5}]".format(quintiles[0],quintiles[1],quintiles[2],quintiles[3],quintiles[4],quintiles[5])
+		print "\n"
 		#print "Average:", sum(self.earnings)/len(self.earnings), " Best:", max(self.earnings), " Worst:", min(self.earnings),"\n" 
 
 if __name__ == "__main__":
 	season = 20152016
 	home_prob = .5
-	goodness = 1.
+	goodness = 0.
 	silent = True
-	num_sims = 10000
-	BuildHistograms(season,home_prob,goodness,silent,num_sims)
+	num_sims = 5000
+	books=['Pinnacle']
+	BuildHistograms(season,books,home_prob,goodness,silent,num_sims)
 
 
